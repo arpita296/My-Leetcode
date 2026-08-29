e604d91e270da4029d3c749165cf1983a78389cb
Arpita Goswami <goswamiarpita004@gmail.com>
2026-08-29 17:50:19 +0530

    Time: 55 ms (43.26%), Space: 68.4 MB (90.44%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+       
+        Map<Integer,Integer>map=new HashMap<>();
+        int max=0;
+        int i=0,j=0;
+        
+        while(j<fruits.length){
+            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+            if(map.size()>2){
+                
+                map.put(fruits[i],map.get(fruits[i])-1);
+                if(map.get(fruits[i])==0){
+                    map.remove(fruits[i]);
+                }
+                
+                i++;
+
+            }
+            max=Math.max(max,j-i+1);
+            j++;
+            
+
+        }
+        
+        return max;
+    }
+}
