f645fa3784561efae8045cf84d3a3e6924f8f152
Arpita Goswami <goswamiarpita004@gmail.com>
2026-08-29 09:54:06 +0530

    Time: 571 ms (5.27%), Space: 142.9 MB (28.02%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,15 +1,34 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        
-        Set <String> seen=new HashSet<>();
+        int k=10;
+        int rep=0;
+        Set <Integer> seen=new HashSet<>();
         Set <String> result=new HashSet<>();
-        for(int i=0;i<=s.length()-10;i++){
-            String dna=s.substring(i,i+10);
-            if(seen.contains(dna)){
-                result.add(dna);
+        Map<Character,Integer>map=new HashMap<>();
+        map.put('A',0);
+        map.put('C',1);
+        map.put('G',2);
+        map.put('T',3);
+        //0123456789
+        //AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+        //9876543210
+        //
+        if(s.length()<k){
+            return new ArrayList<>();
+        }
+        for(int i=0;i<k;i++){
+            rep=rep+((int) Math.pow(4,k-i-1)*map.get(s.charAt(i)));
+        }
+        seen.add(rep);
+        for(int i=k;i<s.length();i++){
+            rep=rep-((int) Math.pow(4,(k-1))*map.get(s.charAt(i-k)));
+            rep=rep*4;
+            rep=rep+map.get(s.charAt(i));
+            if(seen.contains(rep)){
+                result.add(s.substring(i-k+1,i+1));
+                
             }
-            seen.add(dna);
-
+            seen.add(rep);
             
         }
         return new ArrayList<>(result);
